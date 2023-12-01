374de86a87e5991a0d2fa05b3f78edc32a347efd
Zaur Omarow <113785827+Z0RIK@users.noreply.github.com>
2023-12-01 18:41:04 +0600

    Create day_1.cpp

---
+++ src/day_1.cpp
@@ -0,0 +1,83 @@
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <iostream>
+#include <array>
+
+std::array<std::string, 9> numbers = {
+    "one",
+    "two",
+    "three", 
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine"
+};
+
+bool compare(char* c, const std::string& number)
+{
+    size_t i = 0;
+
+    while (*c != '\n' && *c != '\0' && i < number.size() && *c == number[i])
+    {
+        c++;
+        i++;
+    }
+
+    if(i == number.size()) 
+        return true;
+    else 
+        return false;
+}
+
+int main(char argc, char* argv[])
+{
+
+    std::ifstream inputFile;
+    std::stringstream inputStream;
+    std::string inputString;
+
+    if(argc > 1)
+        inputFile.open(argv[1]);
+
+    if(!inputFile.is_open()) return 0;
+    inputStream << inputFile.rdbuf();
+    inputString = inputStream.str();
+
+    inputFile.close();
+
+    size_t first{69}, second{69}, result{};
+
+    for(size_t i = 0; i < inputString.size(); i++)
+    {
+        char c = inputString[i];
+        if (std::isdigit(c))
+        {
+            if (first == 69)
+                first = c - '0';
+            second = c - '0';
+        }
+        else if (c == '\n' || c == '\0')
+        {
+            result += first * 10 + second;
+            first = second = 69;
+        }
+        else
+        {
+            for(size_t j = 0; j < numbers.size(); j++)
+                if (compare(&inputString[i], numbers[j]))
+                {
+                    if (first == 69)
+                        first = j + 1;
+                    second = j + 1;
+                }
+        }
+    }
+    
+    result += first * 10 + second;
+
+    std::cerr << result << std::endl; 
+    return 0;
+}
